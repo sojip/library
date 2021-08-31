04fcfa3e193a74fe05726d3836e0246874111454
sojip <sojipquantin@gmail.com>
2021-08-31 12:10:09 +0100

    added firestore

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
+import {
+  doc,
+  getDoc,
+  getDocs,
+  getFirestore,
+  onSnapshot,
+  query,
+  deleteDoc,
+} from "firebase/firestore";
+import { collection, setDoc } from "firebase/firestore";
 import "./reset.css";
 import "./style.css";
 import logo from "./img/books.png";
 
-let myLibrary = [];
 let newBook = document.querySelector("#newBook");
 let modal = document.querySelector(".modal");
 let closeModal = document.querySelector("#closeButton");
 let formBook = document.querySelector("#formBook");
-let nav = document.querySelector("nav");
 document.querySelector("#logo").src = logo;
 
 class Book {
@@ -22,68 +29,39 @@ class Book {
   }
 }
 
-function addBookToLibrary(title, author, numPages, readState) {
-  let book = new Book(title, author, numPages, readState);
-  myLibrary.unshift(book);
-  return;
-}
-
-function addToDisplay() {
+function addToDisplay(books) {
   //delete previous content
-  let books = document.querySelectorAll(".books");
-  books.forEach((book) => {
-    book.remove();
-  });
-  //add library content
-  myLibrary.forEach(function (book) {
-    let container = document.querySelector("#booksContainer");
-    let div = document.createElement("div");
-    div.setAttribute("data-index", myLibrary.indexOf(book));
-    div.innerHTML = `<h2  class="bookTitle">${book.title}</h2>
+  try {
+    let books_ = document.querySelectorAll(".books");
+    books_.forEach((book) => {
+      book.remove();
+    });
+  } finally {
+    // add library content
+    books.forEach(function (book) {
+      let container = document.querySelector("#booksContainer");
+      let div = document.createElement("div");
+      div.setAttribute("data-id", book.title);
+      div.innerHTML = `<h2  class="bookTitle">${book.title}</h2>
                         <h3>${book.author}</h3>
                         <h3>${book.numPages} Pages</h3>
                         <h3>${book.readState}</h3>
                         <button class="modifyButton">Edit</button><br>
                        <button class="delete">Delete</button>`;
-    div.classList.add("books");
-    container.appendChild(div);
-    requestAnimationFrame(() => {
-      div.classList.add("created");
+      div.classList.add("books");
+      container.appendChild(div);
+      requestAnimationFrame(() => {
+        div.classList.add("created");
+      });
     });
-  });
+  }
   return;
 }
 
 window.addEventListener("click", (e) => {
-  if (e.target.classList.value === "modifyButton") {
-    //modify created Books
-    let arrIndex = Number(e.target.parentNode.dataset.index);
-    let book = myLibrary[arrIndex];
-    modal.classList.add("opened");
-    let title = document.querySelector("#title");
-    let author = document.querySelector("#author");
-    let numPages = document.querySelector("#numPages");
-    let readState = document.querySelector("#readState");
-    let index = document.querySelector("#index");
-    title.value = book.title;
-    author.value = book.author;
-    numPages.value = book.numPages;
-    index.value = arrIndex;
-    book.readState === "Read"
-      ? (readState.checked = true)
-      : (readState.checked = false);
-  } else if (e.target.classList.value === "delete") {
-    let arrIndex = Number(e.target.parentNode.dataset.index);
-    delete myLibrary[arrIndex];
-    requestAnimationFrame(() => {
-      e.target.parentNode.classList.add("removed");
-    });
-    e.target.parentNode.addEventListener("animationend", () => {
-      e.target.parentNode.remove();
-    });
-  } else if (e.target == modal) {
-    let index = document.querySelector("#index").value;
-    if (index) document.querySelector("#index").value = "";
+  if (e.target.classList.value === "modifyButton") showBookDetails(e);
+  else if (e.target.classList.value === "delete") deleteBook(e);
+  else if (e.target == modal) {
     formBook.reset();
     modal.classList.remove("opened");
   }
@@ -95,6 +73,7 @@ newBook.addEventListener("click", () => {
 
 closeModal.addEventListener("click", () => {
   modal.classList.remove("opened");
+  formBook.reset();
 });
 
 formBook.addEventListener("submit", (e) => {
@@ -102,24 +81,13 @@ formBook.addEventListener("submit", (e) => {
   let title = document.querySelector("#title").value;
   let author = document.querySelector("#author").value;
   let numPages = document.querySelector("#numPages").value;
-  let index = document.querySelector("#index").value;
   let readState;
   document.querySelector("#readState").checked === true
     ? (readState = "Read")
     : (readState = "Not read");
   formBook.reset();
   modal.classList.remove("opened");
-
-  if (index) {
-    myLibrary[index].title = title;
-    myLibrary[index].author = author;
-    myLibrary[index].numPages = numPages;
-    myLibrary[index].readState = readState;
-    document.querySelector("#index").value = "";
-  } else {
-    addBookToLibrary(title, author, numPages, readState);
-  }
-  addToDisplay();
+  addBookToLibrary(title, author, numPages, readState);
 });
 
 window.addEventListener("wheel", function (e) {
@@ -127,7 +95,7 @@ window.addEventListener("wheel", function (e) {
   else booksContainer.scrollLeft -= 150;
 });
 
-// Your web app's Firebase configuration
+//Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBHfaphxdnoxA6tN6Ue02a6iqhEKCJuT5k",
   authDomain: "library-2cf5c.firebaseapp.com",
@@ -139,4 +107,63 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+const db = getFirestore();
+
+// Firestore data converter
+const bookConverter = {
+  toFirestore: (book) => {
+    return {
+      title: book.title,
+      author: book.author,
+      numPages: book.numPages,
+      readState: book.readState,
+    };
+  },
+  fromFirestore: (snapshot, options) => {
+    const data = snapshot.data(options);
+    return new Book(data.title, data.author, data.numPages, data.readState);
+  },
+};
+
+async function addBookToLibrary(title, author, numPages, readState) {
+  const book = new Book(title, author, numPages, readState);
+  const ref = doc(db, "books", book.title).withConverter(bookConverter);
+  await setDoc(ref, book, { merge: true });
+  return;
+}
+
+//query to get all the books and listen on change
+const q = query(collection(db, "books"));
+const unsubscribe = onSnapshot(q, (querySnapshot) => {
+  const books = [];
+  querySnapshot.forEach((doc) => {
+    books.push(doc.data());
+  });
+  addToDisplay(books);
+});
+
+async function showBookDetails(e) {
+  //modify created Books
+  let id = e.target.parentNode.dataset.id;
+  const docRef = doc(db, "books", id);
+  let docSnap = await getDoc(docRef);
+  let book = docSnap.data();
+  modal.classList.add("opened");
+  let title = document.querySelector("#title");
+  let author = document.querySelector("#author");
+  let numPages = document.querySelector("#numPages");
+  let readState = document.querySelector("#readState");
+  title.value = await book.title;
+  author.value = await book.author;
+  numPages.value = await book.numPages;
+  (await book.readState) === "Read"
+    ? (readState.checked = true)
+    : (readState.checked = false);
+  return;
+}
+
+async function deleteBook(e) {
+  let id = e.target.parentNode.dataset.id;
+  await deleteDoc(doc(db, "books", id));
+  return;
+}
